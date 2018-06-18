cd3c140ab0935d51d16499ed83b3d1e6d3fd25e0
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2018-06-18 13:09:01 +0100

    Don’t wrap FacetPropTypes in PropTypes.shape to make it extendible

--- src/ResultPropTypes.js
+++ src/ResultPropTypes.js
@@ -1,14 +1,14 @@
 import PropTypes from 'prop-types'
 
-const FacetPropTypes = PropTypes.shape({
+const FacetPropTypes = {
   group: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired
-})
+}
 
 const ResultPropTypes = PropTypes.shape({
   element: PropTypes.object.isRequired,
-  facets: PropTypes.arrayOf(FacetPropTypes)
+  facets: PropTypes.arrayOf(PropTypes.shape(FacetPropTypes))
 })
 
 export {FacetPropTypes, ResultPropTypes}
