2798745abe98b3375855f9a302b07dd23bdf378a
lingyun1010 <zhaolingyun1010@gmail.com>
2018-08-16 16:45:33 +0100

    Add fetchLoader demo props EpisodCard&species

--- html/fetchLoaderDemo.js
+++ html/fetchLoaderDemo.js
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import FetchLoader from '../src/FetchLoader'
+import ExperimentCard from './ExperimentCard'
 
 const render = (options, target) => {
-  ReactDOM.render(<FetchLoader {...options} />, document.getElementById(target))
+  ReactDOM.render(<FetchLoader {...options} ResultElementClass={ExperimentCard} species={``}/>, document.getElementById(target))
 }
 
 export {render}
