f72ce43ee89d60b747043b6efe5207c196bff740
lingyun1010 <zhaolingyun1010@gmail.com>
2018-12-06 17:04:05 +0000

    Amend container and list test with experiment card

--- __test__/FilterList.test.js
+++ __test__/FilterList.test.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import Enzyme from 'enzyme'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
-import { episodes, EpisodesHeader, EpisodeCard } from './TestUtils'
+import { episodes, ExperimentTableHeader, ExperimentTableCard } from './TestUtils'
 
 import FilterList from '../src/FilterList'
 
@@ -12,16 +12,17 @@ Enzyme.configure({ adapter: new Adapter() })
 
 const props = {
   filteredResults: episodes,
-  ResultsHeaderClass: EpisodesHeader,
-  ResultElementClass: EpisodeCard
+  ResultsHeaderClass: ExperimentTableHeader,
+  ResultElementClass: ExperimentTableCard
 }
 
 describe(`FilterList`, () => {
   test(`renders as many components of ResultElementClass as filtered results`, () => {
     const randomFilteredResults = episodes.filter(() => Math.random() > 0.5)
-    const wrapper = shallow(<FilterList {...props} filteredResults={randomFilteredResults}/>)
-    expect(wrapper.find(EpisodesHeader)).toHaveLength(1)
-    expect(wrapper.find(EpisodeCard)).toHaveLength(randomFilteredResults.length)
+    console.log(Object.keys(ExperimentTableHeader().titles).length)
+    const wrapper = mount(<FilterList {...props} filteredResults={randomFilteredResults}/>)
+    expect(wrapper.find(`#title`)).toHaveLength(Object.keys(ExperimentTableHeader().titles).length)
+    expect(wrapper.find(ExperimentTableCard)).toHaveLength(randomFilteredResults.length)
   })
 
   test(`matches snapshot`, () => {
