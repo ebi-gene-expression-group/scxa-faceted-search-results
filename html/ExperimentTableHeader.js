b299d872ad467ae356e7bfb6c549c8de69ab21a8
lingyun1010 <zhaolingyun1010@gmail.com>
2018-12-06 17:05:05 +0000

    Add TableCard and TableHeader files as demo input

---
+++ html/ExperimentTableHeader.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const CardContainerDiv = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex !important;
+  flex-wrap: nowrap;
+  align-items: center;
+  padding: 1rem;
+  font-weight: bolder;
+  font-size: 0.9rem;
+`
+
+const IconDiv = styled.div`
+  width: 15%;
+  text-align: center;
+`
+
+const MarkerDiv = styled.div`
+  width: 15%;
+  text-align: center;
+`
+
+const TitleDiv = styled.div`
+  width: 40%;
+  text-align: center;
+`
+
+const VariableDiv = styled.div`
+  width: 20%;
+  text-align: center;
+`
+
+const CountDiv = styled.div`
+  width: 10%;
+  text-align: center;
+`
+
+const ExperimentTableHeader = () => 
+({  
+    'container': CardContainerDiv,
+    'titles': {
+      'Species': IconDiv,
+      'Marker genes': MarkerDiv,
+      'Title': TitleDiv,
+      'Experimental variables': VariableDiv,
+      'Number of assays': CountDiv}
+})
+
+export default ExperimentTableHeader
