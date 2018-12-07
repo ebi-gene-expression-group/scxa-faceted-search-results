1a03643a909cb787368bdd8249f9afed030e030b
lingyun1010 <zhaolingyun1010@gmail.com>
2018-12-07 14:43:56 +0000

    Add test for the user selection on table headers

--- __test__/FilterList.test.js
+++ __test__/FilterList.test.js
@@ -4,7 +4,7 @@ import Enzyme from 'enzyme'
 import { shallow, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
-import { episodes, ExperimentTableHeader, ExperimentTableCard } from './TestUtils'
+import { getRandomInt, episodes, ExperimentTableHeader, ExperimentTableCard } from './TestUtils'
 
 import FilterList from '../src/FilterList'
 
@@ -18,11 +18,27 @@ const props = {
 
 describe(`FilterList`, () => {
   test(`renders as many components of ResultElementClass as filtered results`, () => {
-    const randomFilteredResults = episodes.filter(() => Math.random() > 0.5)
-    console.log(Object.keys(ExperimentTableHeader().titles).length)
-    const wrapper = mount(<FilterList {...props} filteredResults={randomFilteredResults}/>)
+    const wrapper = mount(<FilterList {...props} filteredResults={episodes} />)
     expect(wrapper.find(`#title`)).toHaveLength(Object.keys(ExperimentTableHeader().titles).length)
-    expect(wrapper.find(ExperimentTableCard)).toHaveLength(randomFilteredResults.length)
+    expect(wrapper.find(ExperimentTableCard)).toHaveLength(episodes.length)
+  })
+
+  test(`highlights the user selected table header`, () => {
+    const randomFilteredResults = episodes.filter(() => Math.random() > 0.5)
+    const titleNames = Object.keys(ExperimentTableHeader().titles)
+    const wrapper = mount(<FilterList {...props} filteredResults={randomFilteredResults} />)
+    const mockState = (str) => {
+      const splitStr = str.toLowerCase().split(' ').map((string, index)=>
+        index === 0 ?
+          string.charAt(0).toLowerCase() + string.substring(1) :
+          string.charAt(0).toUpperCase() + string.substring(1)    
+      )
+      return splitStr.join('')
+    }
+    wrapper.setState({sortTitle: mockState(titleNames[getRandomInt(0, titleNames.length)])})
+    console.log(wrapper.state())
+    expect(wrapper.find(`#title`)).toHaveLength(1)
+    expect(wrapper.find(`#selected`)).toHaveLength(1)
   })
 
   test(`matches snapshot`, () => {
