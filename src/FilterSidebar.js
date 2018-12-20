2c7693089c8537733fe64dad513b3330279bc4fb
lingyun1010 <zhaolingyun1010@gmail.com>
2018-12-20 16:47:25 +0000

    Reformat FilterSidebar by pass cleaned facetGroups

--- src/FilterSidebar.js
+++ src/FilterSidebar.js
@@ -1,36 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import _ from 'lodash'
 
 import CheckboxFacetGroup from './facetgroups/CheckboxFacetGroup'
 import MultiselectDropdownFacetGroup from './facetgroups/MultiselectDropdownFacetGroup'
 
-import {FacetPropTypes, FacetTooltipPropTypes} from './ResultPropTypes'
+import {FacetPropTypes} from './ResultPropTypes'
 
-const FilterSidebar = ({facets, checkboxFacetGroups, onChange}) => {
-  const facetGroups =
-    _(facets)
-      .sortBy([`group`, `label`])
-      .groupBy(`group`)
-      .toPairs()
-      .partition((facetGroup) => checkboxFacetGroups.includes(facetGroup[0]))
-      .value()
+const FilterSidebar = ({facetGroups, onChange}) => {
 
   // Facets as checkboxes go first by design
   return(
     [
       facetGroups[0]
-        .map((facetGroup) => <CheckboxFacetGroup facetGroupName={facetGroup[0]}
-                                                 facetGroupNameDescription={facetGroup[1][0].description}
-                                                 facets={facetGroup[1]}
-                                                 onChange={onChange}
-                                                 key={facetGroup[0]} />),
+        .map((facetGroup) => facetGroup &&
+             <CheckboxFacetGroup facetGroupName={facetGroup[0]}
+               facetGroupNameDescription={facetGroup[1][0].description}
+               facets={facetGroup[1]}
+               onChange={onChange}
+               key={facetGroup[0]} />),
       facetGroups[1]
-        .map((facetGroup) => <MultiselectDropdownFacetGroup facetGroupName={facetGroup[0]}
-                                                            facetGroupNameDescription={facetGroup[1][0].description}
-                                                            facets={facetGroup[1]}
-                                                            onChange={onChange}
-                                                            key={facetGroup[0]} />)
+        .map((facetGroup) =>
+          <MultiselectDropdownFacetGroup facetGroupName={facetGroup[0]}
+            facetGroupNameDescription={facetGroup[1][0].description}
+            facets={facetGroup[1]}
+            onChange={onChange}
+            key={facetGroup[0]} />)
     ]
   )
 }
