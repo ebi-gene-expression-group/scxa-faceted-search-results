a8f07389d25b669f8d2aefbbf0eba99afb8ff675
lingyun1010 <zhaolingyun1010@gmail.com>
2018-12-21 14:00:20 +0000

    Reformat the FilterSidebar with tidy facet groups

--- src/FilterSidebar.js
+++ src/FilterSidebar.js
@@ -1,24 +1,37 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
 import CheckboxFacetGroup from './facetgroups/CheckboxFacetGroup'
 import MultiselectDropdownFacetGroup from './facetgroups/MultiselectDropdownFacetGroup'
+import _ from "lodash"
 
-import {FacetPropTypes} from './ResultPropTypes'
+import {ResultPropTypes} from './ResultPropTypes'
 
-const FilterSidebar = ({facetGroups, onChange}) => {
+const FilterSidebar = ({facets, checkboxFacetGroups, results, onChange}) => {
+  const facetGroups =
+      _(facets)
+        .sortBy([`group`, `label`])
+        .groupBy(`group`)
+        .toPairs()
+        .partition((facetGroup) => checkboxFacetGroups.includes(facetGroup[0]))
+        .value()
 
+  const cleanCheckboxFacetGroups = facetGroups[0].map(checkFacet => {
+    const facet = checkFacet[0]===`Marker genes` ? `markerGenes` : `species`
+    return !results.every((result,index,arr) => result.element[facet]===arr[0].element[facet]) && checkFacet
+  })
+
+  const cleanFacetGroups =  [cleanCheckboxFacetGroups ? cleanCheckboxFacetGroups : [], facetGroups[1]]
   // Facets as checkboxes go first by design
   return(
     [
-      facetGroups[0]
+      cleanFacetGroups[0]
         .map((facetGroup) => facetGroup &&
              <CheckboxFacetGroup facetGroupName={facetGroup[0]}
                facetGroupNameDescription={facetGroup[1][0].description}
                facets={facetGroup[1]}
                onChange={onChange}
                key={facetGroup[0]} />),
-      facetGroups[1]
+      cleanFacetGroups[1]
         .map((facetGroup) =>
           <MultiselectDropdownFacetGroup facetGroupName={facetGroup[0]}
             facetGroupNameDescription={facetGroup[1][0].description}
@@ -30,15 +43,10 @@ const FilterSidebar = ({facetGroups, onChange}) => {
 }
 
 FilterSidebar.propTypes = {
-  facets: PropTypes.arrayOf(PropTypes.shape({
-    ...FacetPropTypes,
-    disabled: PropTypes.bool.isRequired})).isRequired,
+  facets: PropTypes.arrayOf(PropTypes.array).isRequired,
   checkboxFacetGroups: PropTypes.arrayOf(PropTypes.string),
+  results: PropTypes.arrayOf(ResultPropTypes).isRequired,
   onChange: PropTypes.func.isRequired
-}
-
-FilterSidebar.defaultProps = {
-  checkboxFacetGroups: []
 }
 
 export default FilterSidebar
